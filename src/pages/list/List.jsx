a5bd47239fc197018c4bbdabcec61b20c888b0ae
AhmedYasser155 <75222161+AhmedYasser155@users.noreply.github.com>
2022-07-05 21:24:10 +0200

    seach bar is done. Note: I mistakenly didn't create a branch before

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import Header from '../../components/header/Header';
-import Navbar from '../../components/navbar/Navbar';
+import React, { Children, useState } from "react";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
 import "./list.css";
-import SearchItem from '../../components/searchItem/SearchItem';
-
+import SearchItem from "../../components/searchItem/SearchItem";
+import { useLocation } from "react-router-dom";
+import { DateRange } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { format } from "date-fns";
 const List = () => {
+  const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [options, setOptions] = useState(location.state.options);
+  const [openDate, setOpenDate] = useState(false);
   return (
     <div>
       <Navbar type="list" />
@@ -12,7 +21,73 @@ const List = () => {
       <div className="listContainer">
         <div className="listWrapper">
           <div className="listSearch">
-            Search
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label>Destination</label>
+              <input placeholder={destination} type="text" />
+            </div>
+            <div className="lsItem">
+              <label>Check-in Date</label>
+              <span onClick={() => setOpenDate(!openDate)}>{`${format(
+                date[0].startDate,
+                "dd/MM/yyyy"
+              )} to ${format(date[0].endDate, "dd/MM/yyyy")}`}</span>
+              {openDate && (
+                <DateRange
+                  className="dataInput"
+                  editableDateInputs={true}
+                  onChange={(item) => setDate([item.selection])}
+                  minDate={new Date()}
+                  ranges={date}
+                />
+              )}
+
+              <div className="lsItem">
+                <label>Options</label>
+                <div className="lsOptions">
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">
+                      Min Price <small>per night</small>
+                    </span>
+                    <input min={0} type="number" className="lsOptionInput" />
+                  </div>
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">
+                      Max Price <small>per night</small>
+                    </span>
+                    <input min={0} type="number" className="lsOptionInput" />
+                  </div>
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">Adults</span>
+                    <input
+                      min={1}
+                      type="number"
+                      className="lsOptionInput"
+                      placeholder={options.adults}
+                    />
+                  </div>
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">Children</span>
+                    <input
+                      min={0}
+                      type="number"
+                      className="lsOptionInput"
+                      placeholder={options.children}
+                    />
+                  </div>
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">Rooms</span>
+                    <input
+                      min={1}
+                      type="number"
+                      className="lsOptionInput"
+                      placeholder={options.rooms}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button>Search</button>
           </div>
           <div className="listResults">
             <SearchItem />
@@ -26,7 +101,7 @@ const List = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
